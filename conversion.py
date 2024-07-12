17a29bf8fcf75d63d169d169b8b615588f172590
temilaw <126417288+temilaw@users.noreply.github.com>
2024-07-12 17:28:24 +0100

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,30 +1,24 @@
-"""declare the variables num1, num2, num3 and string1
-convert num1 to an integer
-declare as int_num1
-convert num2 to a float
-declare as float_num2
-convert num3 to a string
-declare as str_num3
-convert string1 to an integer
-declare as int_string1
-print variables on separate lines
-"""
+# Declare the variables
 num1 = 99.23
 num2 = 23
 num3 = 150
 string1 = "100"
 
+# Convert num1 to an integer and assign to int_num1
 int_num1 = int(num1)
+
+# Convert num2 to a float and assign to float_num2
 float_num2 = float(num2)
+
+# Convert num3 to a string and assign to str_num3
 str_num3 = str(num3)
+
+# Convert string1 to an integer and assign to int_string1
 int_string1 = int(string1)
 
+# Print the variables on separate lines
 print(int_num1)
 print(float_num2)
 print(str_num3)
 print(int_string1)
-
-
-
-
 
